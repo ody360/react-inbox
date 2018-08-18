4f5c922ddd047d78b941406f7db240d42e328f08
Hedim Ramirez <ody360@gmail.com>
2018-08-18 07:45:22 -0700

    populating messages through collective api

--- src/App.js
+++ src/App.js
@@ -1,75 +1,95 @@
 import React, { Component } from 'react';
 import Toolbar from './components/Toolbar';
 import MessageList from './components/MessageList'
+import axios from 'axios'
+
 
 class App extends Component {
   constructor() {
     super()
 
     this.state = {
-      messages:
-      [ 
-        {
-          "id": 1,
-          "subject": "You can't input the protocol without calculating the mobile RSS protocol!",
-          "read": false,
-          "starred": true,
-          "labels": ["dev", "personal"]
-        },
-        {
-          "id": 2,
-          "subject": "connecting the system won't do anything, we need to input the mobile AI panel!",
-          "read": false,
-          "starred": false,
-          "selected": true,
+      messages: [{
+          "id": '',
+          "subject": '',
+          "read":false, 
+          "starred":false, 
           "labels": []
-        },
-        {
-          "id": 3,
-          "subject": "Use the 1080p HTTP feed, then you can parse the cross-platform hard drive!",
-          "read": false,
-          "starred": true,
-          "labels": ["dev"]
-        },
-        {
-          "id": 4,
-          "subject": "We need to program the primary TCP hard drive!",
-          "read": true,
-          "starred": false,
-          "selected": true,
-          "labels": []
-        },
-        {
-          "id": 5,
-          "subject": "If we override the interface, we can get to the HTTP feed through the virtual EXE interface!",
-          "read": false,
-          "starred": false,
-          "labels": ["personal"]
-        },
-        {
-          "id": 6,
-          "subject": "We need to back up the wireless GB driver!",
-          "read": true,
-          "starred": true,
-          "labels": []
-        },
-        {
-          "id": 7,
-          "subject": "We need to index the mobile PCI bus!",
-          "read": true,
-          "starred": false,
-          "labels": ["dev", "personal"]
-        },
-        {
-          "id": 8,
-          "subject": "If we connect the sensor, we can get to the HDD port through the redundant IB firewall!",
-          "read": true,
-          "starred": true,
-          "labels": []
-        }
-      ]
+        }]
+      // [ 
+      //   {
+      //     "id": 1,
+      //     "subject": "You can't input the protocol without calculating the mobile RSS protocol!",
+      //     "read": false,
+      //     "starred": true,
+      //     "labels": ["dev", "personal"]
+      //   },
+      //   {
+      //     "id": 2,
+      //     "subject": "connecting the system won't do anything, we need to input the mobile AI panel!",
+      //     "read": false,
+      //     "starred": false,
+      //     "selected": true,
+      //     "labels": []
+      //   },
+      //   {
+      //     "id": 3,
+      //     "subject": "Use the 1080p HTTP feed, then you can parse the cross-platform hard drive!",
+      //     "read": false,
+      //     "starred": true,
+      //     "labels": ["dev"]
+      //   },
+      //   {
+      //     "id": 4,
+      //     "subject": "We need to program the primary TCP hard drive!",
+      //     "read": true,
+      //     "starred": false,
+      //     "selected": true,
+      //     "labels": []
+      //   },
+      //   {
+      //     "id": 5,
+      //     "subject": "If we override the interface, we can get to the HTTP feed through the virtual EXE interface!",
+      //     "read": false,
+      //     "starred": false,
+      //     "labels": ["personal"]
+      //   },
+      //   {
+      //     "id": 6,
+      //     "subject": "We need to back up the wireless GB driver!",
+      //     "read": true,
+      //     "starred": true,
+      //     "labels": []
+      //   },
+      //   {
+      //     "id": 7,
+      //     "subject": "We need to index the mobile PCI bus!",
+      //     "read": true,
+      //     "starred": false,
+      //     "labels": ["dev", "personal"]
+      //   },
+      //   {
+      //     "id": 8,
+      //     "subject": "If we connect the sensor, we can get to the HDD port through the redundant IB firewall!",
+      //     "read": true,
+      //     "starred": true,
+      //     "labels": []
+      //   }
+      // ]
     }
 
+  }
+
+  componentDidMount() {
+    this.getMessages()
+  }
+
+  getMessages = async () => {
+    const res = await axios.get('http://localhost:8082/api/messages')
+    console.log(res.data)
+    this.setState({
+      messages: res.data
+    })
   }
 
   selectMessage = (id) => {
@@ -97,14 +117,15 @@ class App extends Component {
 
 
   selectAll = () => {
-  
-    const allMessages = this.state.messages.map(message => {
+    const allMessages = this.state.messages.every(message => message.selected === true)
+
+    const highlightedMsgs = this.state.messages.map(message => {
       allMessages ? delete message.selected : message.selected = true
       return message;
     });
 
     this.setState({
-      messages: allMessages
+      messages: highlightedMsgs
     })
   }
 
